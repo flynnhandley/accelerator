381158048c99efba8ab1fb3aa0e23667a1ac9794
Flynn Handley <FHandley+aglemu@agl.com.au>
2025-10-26 14:40:59 +1100

    .

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Enable serving static files from wwwroot
+app.UseDefaultFiles(); // Serves index.html by default
+app.UseStaticFiles();
+
+// Keep API endpoint available at /api/hello
+app.MapGet("/api/hello", () => "Hello World!");
 
 app.Run();
